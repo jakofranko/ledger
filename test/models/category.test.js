c53eb107839eff419ea753197ea2791a9f48c1e1
Jacob Daniel Franklin <jacob.d.franklin@gmail.com>
2018-08-02 14:35:16 -0700

    tweaks and fixes to unit tests

--- test/models/category.test.js
+++ test/models/category.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const Category = require('../../models/category');
 const CATEGORY_NAME = 'TestCategoryForUnitTests';
+const UPDATED_CATEGORY_NAME = 'NEWCATEGORYNAME';
 let TEST_CATEGORY;
 
 describe('Category model', function() {
@@ -22,20 +23,25 @@ describe('Category model', function() {
 
     afterEach('remove test categories from DB', function(done) {
         let cat = new Category();
+        function deleteCategories(queue) {
+            return new Promise((resolve, reject) => {
+                let currentCategory;
+                if (queue && queue.length) {
+                    currentCategory = queue.pop();
+                    cat.delete(currentCategory.id)
+                    .then(() => resolve(deleteCategories(queue)))
+                    .catch(err => reject(err));
+                } else {
+                    resolve();
+                }
+            });
+        }
+
         cat.getAll()
             .then(categories => {
-                let deletePromises = [];
-                categories.filter(category => category.name === CATEGORY_NAME).forEach(category => {
-                    deletePromises.push(cat.delete(category.id));
-                });
-
-                Promise.all(deletePromises)
-                    .then(function(deleteResults) {
-                        assert(deleteResults.length);
-                    })
-                    .catch(err => {
-                        assert.fail(err);
-                    });
+                let deleteQueue = categories.filter(category => category.name === CATEGORY_NAME || category.name === UPDATED_CATEGORY_NAME);
+                assert(deleteQueue.length);
+                return deleteCategories(deleteQueue);
             })
             .catch(err => {
                 assert.fail(err);
@@ -65,9 +71,32 @@ describe('Category model', function() {
 
     });
 
+    it('should update a single category', function(done) {
+        const cat = new Category();
+        cat.update(TEST_CATEGORY.id, {
+            name: UPDATED_CATEGORY_NAME
+        })
+        .then(category => {
+            cat.get(TEST_CATEGORY.id)
+            .then(c => {
+                assert(c.name === UPDATED_CATEGORY_NAME);
+            })
+            .catch(err => {
+                assert.fail(err)
+            });
+        })
+        .catch(err => {
+            assert.fail(err);
+        })
+        .finally(() => {
+            cat.close();
+            done();
+        });
+    });
+
     it('should get a single cateogry', function(done) {
         const cat = new Category();
-        cat.get(0)
+        cat.get(TEST_CATEGORY.id)
             .then(category => {
                 assert(category.name);
             })
