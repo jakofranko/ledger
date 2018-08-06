ec18fa0fa3d1c77af2def617efa90ab8f04e2fbd
Jacob Daniel Franklin <jacob.d.franklin@gmail.com>
2018-08-06 10:10:08 -0700

    Added unit tests for milestone

---
+++ test/models/milestone.test.js
@@ -0,0 +1,139 @@
+const assert = require('assert');
+const Promise = require('bluebird');
+const Milestone = require('../../models/milestone.js');
+const TEST_MILESTONE = {
+    name: 'Test Milestone',
+    goal_id: 0
+};
+const UPDATED_MILESTONE = {
+    name: "UPDATED TEST MILESTONE",
+    done: 1,
+    date: "01/01/2001",
+    goal_id: 1
+};
+let TEST_MILESTONE_ID;
+
+describe('Milestone model', function() {
+    beforeEach('a test milestone should be created', function(done) {
+        const m = Milestone;
+        m.create(TEST_MILESTONE)
+        .then(milestone => {
+            assert(milestone.name === TEST_MILESTONE.name);
+            TEST_MILESTONE_ID = milestone.id;
+            done();
+        })
+        .catch(err => {
+            assert.fail(err);
+        });
+    });
+
+    afterEach('all test milestones should be deleted', function(done) {
+        const m = Milestone;
+        function deleteMilestones(queue) {
+            return new Promise((resolve, reject) => {
+                let currentMilestone;
+                if (queue && queue.length) {
+                    currentMilestone = queue.pop();
+                    m.delete(currentMilestone.id)
+                    .then(() => resolve(deleteMilestones(queue)))
+                    .catch(err => reject(err));
+                } else {
+                    resolve();
+                }
+            });
+        }
+
+        m.getAll()
+        .then(milestones => {
+            let testMilestones = milestones.filter(milestone => milestone.name === TEST_MILESTONE.name || milestone.name === UPDATED_MILESTONE.name);
+            return deleteMilestones(testMilestones);
+        })
+        .then(() => done())
+        .catch(err => {
+            assert.fail(err);
+            done();
+        })
+    });
+
+    it('should get a single milestone', function(done) {
+        const m = Milestone;
+        m.get(TEST_MILESTONE_ID)
+        .then(milestone => {
+            assert(milestone[0] && milestone[0].name === TEST_MILESTONE.name);
+            done();
+        })
+        .catch(err => {
+            assert.fail(err);
+            done();
+        });
+    });
+
+    it('should get all milestones', function(done) {
+        const m = Milestone;
+        m.getAll()
+        .then(milestones => {
+            assert(milestones.length);
+            assert(milestones[0].name);
+            assert(milestones[0].done !== undefined);
+            assert(milestones[0].goal_id !== undefined);
+            assert(milestones[0].date !== undefined);
+            done();
+        })
+        .catch(err => {
+            assert.fail(err);
+            done();
+        });
+    });
+
+    it('should get all milestones that are done', function(done) {
+        const m = Milestone;
+        m.update(TEST_MILESTONE_ID, UPDATED_MILESTONE).then(ms => {
+            m.getDone()
+            .then(completed => {
+                assert(completed.length);
+                completed.forEach(c => {
+                    assert(c.done === 1);
+                });
+
+                done();
+            })
+            .catch(err => {
+                assert.fail(err);
+                done();
+            });
+        });
+    });
+
+    it('should get all milestones that are not done', function(done) {
+        const m = Milestone;
+        m.getNotDone()
+        .then(incomplete => {
+            assert(incomplete.length);
+            incomplete.forEach(c => {
+                assert(c.done === 0);
+            });
+
+            done();
+        })
+        .catch(err => {
+            assert.fail(err);
+            done();
+        });
+    });
+
+    it('should update a single milestone', function(done) {
+        const m = Milestone;
+        m.update(TEST_MILESTONE_ID, UPDATED_MILESTONE)
+        .then(updatedMilestone => {
+            assert(updatedMilestone && updatedMilestone.name === UPDATED_MILESTONE.name);
+            assert(updatedMilestone && updatedMilestone.done === UPDATED_MILESTONE.done);
+            assert(updatedMilestone && updatedMilestone.date === UPDATED_MILESTONE.date);
+            assert(updatedMilestone && updatedMilestone.goal_id === UPDATED_MILESTONE.goal_id);
+            done();
+        })
+        .catch(err => {
+            assert.fail(err);
+            done();
+        });
+    });
+});
