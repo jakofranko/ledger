1793ce26748e93e566dfd0be40b65a8a1e476ec8
Jacob Daniel Franklin <jacob.d.franklin@gmail.com>
2018-08-07 08:59:33 -0700

    Updated logs router to use new API for log model

--- routes/logs.js
+++ routes/logs.js
@@ -1,55 +1,45 @@
-var express = require('express');
-var router  = express.Router();
-var Log = require('../models/log');
+const express = require('express');
+const router  = express.Router();
+const Log = require('../models/log');
 
 router.get('/', function(req, res, next) {
-    Log
-        .getAll()
-        .then(logs => res.json({ logs }))
-        .catch(err => next(err));
+    const l = new Log();
+    l.getAll()
+    .then(logs => res.json({ logs }))
+    .catch(err => next(err))
+    .finally(() => l.close());
 });
 
 router.post('/', function(req, res, next) {
-    Log
-        .create(req.body)
-        .then(log => res.json(log))
-        .catch(err => next(err));
+    const l = new Log();
+    l.create(req.body)
+    .then(log => res.json(log))
+    .catch(err => next(err))
+    .finally(() => l.close());
 });
 
 router.get('/:id', function(req, res, next) {
-    console.log("==========");
-    console.log("Body: ", req.body);
-    console.log("==========");
-    console.log("Params: ", req.params);
-    console.log("==========");
-    Log
-        .get(req.params.id)
-        .then(log => res.json(log))
-        .catch(err => next(err));
+    const l = new Log();
+    l.get(req.params.id)
+    .then(log => res.json(log))
+    .catch(err => next(err))
+    .finally(() => l.close());
 });
 
 router.put('/:id', function(req, res, next) {
-    console.log("==========");
-    console.log("Body: ", req.body);
-    console.log("==========");
-    console.log("Params: ", req.params);
-    console.log("==========");
-    Log
-        .update(req.params.id, req.body)
-        .then(log => res.json(log))
-        .catch(err => next(err));
+    const l = new Log();
+    l.update(req.params.id, req.body)
+    .then(log => res.json(log))
+    .catch(err => next(err))
+    .finally(() => l.close());
 });
 
 router.delete('/:id', function(req, res, next) {
-    console.log("==========");
-    console.log("Body: ", req.body);
-    console.log("==========");
-    console.log("Params: ", req.params);
-    console.log("==========");
-    Log
-        .delete(req.params.id)
-        .then(log => res.json(log))
-        .catch(err => next(err));
+    const l = new Log();
+    l.delete(req.params.id)
+    .then(log => res.json(log))
+    .catch(err => next(err))
+    .finally(() => l.close());
 });
 
 module.exports = router;
