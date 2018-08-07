f38231c738bb433c1d0889b0c35da58bef9a4e8e
Jacob Daniel Franklin <jacob.d.franklin@gmail.com>
2018-08-07 08:59:56 -0700

    Updated milestones router to use new API for milestone model

--- routes/milestones.js
+++ routes/milestones.js
@@ -1,69 +1,61 @@
 var express = require('express');
 var router  = express.Router();
-var Milestones = require('../models/milestone');
+var Milestone = require('../models/milestone');
 
 router.get('/', function(req, res, next) {
-   Milestones
-        .getAll()
-        .then(milestones => res.json({ milestones }))
-        .catch(err => next(err));
+   const m = new Milestone();
+    m.getAll()
+    .then(milestones => res.json({ milestones }))
+    .catch(err => next(err))
+    .finally(() => m.close());
 });
 
 router.get('/done', function(req, res, next) {
-    Milestones
-        .getDone()
-        .then(milestones => res.json({ milestones }))
-        .catch(err => next(err));
+    const m = new Milestone();
+    m.getDone()
+    .then(milestones => res.json({ milestones }))
+    .catch(err => next(err))
+    .finally(() => m.close());
 });
 
 router.get('/not-done', function(req, res, next) {
-    Milestones
-        .getNotDone()
-        .then(milestones => res.json({ milestones }))
-        .catch(err => next(err));
+    const m = new Milestone();
+    m.getNotDone()
+    .then(milestones => res.json({ milestones }))
+    .catch(err => next(err))
+    .finally(() => m.close());
 });
 
 router.post('/', function(req, res, next) {
-    Milestones
-        .create(req.body)
-        .then(milestone => res.json(milestone))
-        .catch(err => next(err));
+    const m = new Milestone();
+    m.create(req.body)
+    .then(milestone => res.json(milestone))
+    .catch(err => next(err))
+    .finally(() => m.close());
 });
 
 router.get('/:id', function(req, res, next) {
-    console.log("==========");
-    console.log("Body: ", req.body);
-    console.log("==========");
-    console.log("Params: ", req.params);
-    console.log("==========");
-    Milestones
-        .get(req.params.id)
-        .then(milestone => res.json(milestone))
-        .catch(err => next(err));
+    const m = new Milestone();
+    m.get(req.params.id)
+    .then(milestone => res.json(milestone))
+    .catch(err => next(err))
+    .finally(() => m.close());
 });
 
 router.put('/:id', function(req, res, next) {
-    console.log("==========");
-    console.log("Body: ", req.body);
-    console.log("==========");
-    console.log("Params: ", req.params);
-    console.log("==========");
-    Milestones
-        .update(req.params.id, req.body)
-        .then(milestone => res.json(milestone))
-        .catch(err => next(err));
+    const m = new Milestone();
+    m.update(req.params.id, req.body)
+    .then(milestone => res.json(milestone))
+    .catch(err => next(err))
+    .finally(() => m.close());
 });
 
 router.delete('/:id', function(req, res, next) {
-    console.log("==========");
-    console.log("Body: ", req.body);
-    console.log("==========");
-    console.log("Params: ", req.params);
-    console.log("==========");
-    Milestones
-        .delete(req.params.id)
-        .then(milestone => res.json(milestone))
-        .catch(err => next(err));
+    const m = new Milestone();
+    m.delete(req.params.id)
+    .then(milestone => res.json(milestone))
+    .catch(err => next(err))
+    .finally(() => m.close());
 });
 
 module.exports = router;
